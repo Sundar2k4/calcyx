3e6b182a4b378723ed9d08d3eedc4d6b6abd7efd
Sundar2k4 <csundar2004@gmail.com>
2024-10-19 23:19:29 +0530

    initial:changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import React, {useState} from "react";
+import Button from "./components/Button";
+import Input from "./components/Input";
+import * as math from "mathjs";
+const App = () =>
+{
+  const [text, setText] = useState("");
+  const [result, setResult] = useState("");
+  const addtotext = (val) =>
+  {
+      setText((text)=> [...text,val +""]);
+  };
+  const DEL= () =>
+  {
+    setText("");
+    setResult("");
+  };
+   
+  const answer = () =>
+  {
+     const input = text.join("");
+     setResult(math.evaluate(input));
+  };
+  return(
+    
+    <div class="app">
+    <div class="wrapper">
+    <Input text = {text} result={result}/>
+    <div class="row">
+    <Button symbol={7} handleClick={addtotext}/>
+    <Button symbol={8} handleClick={addtotext}/>
+    <Button symbol={9} handleClick={addtotext}/> 
+    <Button symbol={"/"} color={"black"} handleClick={addtotext}/>
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    </div>
+    <div class="row">
+    <Button symbol={4} handleClick={addtotext}/>
+    <Button symbol={5} handleClick={addtotext}/>
+    <Button symbol={6} handleClick={addtotext}/>
+    <Button symbol={"*"} color={"black"}handleClick={addtotext}/>
+    </div>
+    <div class="row">
+    <Button symbol={1} handleClick={addtotext}/>
+    <Button symbol={2} handleClick={addtotext}/>
+    <Button symbol={3} handleClick={addtotext}/>
+    <Button symbol={"+"} color={"black"} handleClick={addtotext}/>
+    </div>
+    <div class="row">
+    <Button symbol={0}  handleClick={addtotext}/>
+    <Button symbol={"."} handleClick={addtotext}/>
+    <Button symbol={"="} handleClick={answer}/>
+    <Button symbol={"-"} color={"black"} handleClick={addtotext}/>
+   
+    </div>
+    <Button symbol={"DEL"} color={"black"} handleClick={DEL}/>
+    </div>
+    </div>
+  );
+};
+export default App;      
---
+++ src/components/Input.jsx
@@ -0,0 +1,18 @@
+import "./Input.css";
+const Input = ({text,result}) =>
+{
+    return(
+        <div class="input-wrapper">
+        <div class="result">
+           <h1>{result}</h1> 
+        </div>
+
+
+        <div class="text">
+            <h3>{text}</h3>
+        </div>
+        </div>
+    ); 
+};
+
+export default Input;
